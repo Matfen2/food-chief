9d9d2309c077ce8b7d61fc04f18df6fd60ba4713
Mathieu FENOUIL <110094556+Matfen2@users.noreply.github.com>
2025-10-24 13:34:32 +0200

    Initialisation du frontend
    - Création des raccourcis couleurs et fonts dans index.css
    - Ajout des fonts spécifiques et des images de recettes dans le dossier public
    - Installation des dépendances react-router-dom, react-icons, tailwindcss, postcss et axios
    - Création d'un dossier frontend via le terminal avec npm create vite@latest frontend -- --template react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom"
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-[#163C2E]">
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
